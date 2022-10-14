4380665eb7a979ee243915136f18422030480367
Diaz_Nugraha <diaznugraha00@gmail.com>
2022-10-15 04:35:14 +0700

    create and update entities

---
+++ entity/Driver.go
@@ -0,0 +1,20 @@
+package entity
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Driver struct {
+	Id             int    `gorm:"primaryKey,not null,autoIncrement;uniqueIndex"`
+	Name           string `gorm:"size:256,not null"`
+	Address        string `gorm:"size:256,not null"`
+	ActivityStatus bool
+	VehicleBrand   string `gorm:"size:100,not null"`
+	VehicleType    string `gorm:"size:100,not null"`
+	LicensePlate   string `gorm:"size:50,not null"`
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
+	DeletedAt      gorm.DeletedAt
+}
--- entity/user.go
+++ entity/user.go
@@ -1,10 +1,21 @@
 package entity
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type User struct {
-	gorm.Model
-	Name     string `gorm:"size:256"`
-	Email    string `gorm:"size:256"`
-	Password string `gorm:"size:256"`
+	Id            int     `gorm:"primaryKey,not null,autoIncrement;uniqueIndex"`
+	OrderId       int     `gorm:"unique"`
+	Order         []Order `gorm:"foreignKey:Id;references:OrderId;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	FirstName     string  `gorm:"size:256"`
+	LastName      string  `gorm:"size:256"`
+	Email         string  `gorm:"size:256"`
+	Password      string  `gorm:"size:256"`
+	RememberToken string  `gorm:"size:256"`
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+	DeletedAt     gorm.DeletedAt
 }
