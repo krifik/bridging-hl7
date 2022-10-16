16f58c1a2a66c90a7486c85205eb745d6efc1f69
Fikri Ilhamsyah <fiqriilhamsyah09@gmail.com>
2022-10-16 23:52:58 +0700

    use default cost for hashing password

--- helper/password.go
+++ helper/password.go
@@ -7,7 +7,7 @@ import (
 )
 
 func ToHashedPassword(password string) string {
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	exception.PanicIfNeeded(err)
 	return string(hashedPassword)
 }
