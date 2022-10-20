a8fcf2956d2f4021ec2c0a8c3e48226794148e00
Diaz_Nugraha <diaznugraha00@gmail.com>
2022-10-20 20:10:32 +0700

    configure entities for db migration

--- config/model_migration.go
+++ config/model_migration.go
@@ -9,6 +9,12 @@ type Entity struct {
 func RegisterEntities() []Entity {
 	return []Entity{
 		{Entity: entity.User{}},
+		{Entity: entity.Order{}},
+		{Entity: entity.Driver{}},
+		{Entity: entity.Payment{}},
 		{Entity: entity.Product{}},
+		{Entity: entity.ProductImage{}},
+		{Entity: entity.Category{}},
+		{Entity: entity.Partner{}},
 	}
 }
