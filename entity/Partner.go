b85a6abbe98126dc47e8e49d52e11a1c99658d21
Fikri Ilhamsyah <fiqriilhamsyah09@gmail.com>
2022-11-01 00:08:44 +0700

    change unique to index key

--- entity/Partner.go
+++ entity/Partner.go
@@ -7,14 +7,14 @@ import (
 )
 
 type Partner struct {
-	ID        int       `gorm:"primaryKey,not null,autoIncrement;uniqueIndex"`
-	ProductId int       `gorm:"unique"`
-	Product   []Product `gorm:"foreignKey:ID;references:ProductId;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
-	Location  string    `gorm:"size:100"`
-	Name      string    `gorm:"size:100"`
-	Desc      string    `gorm:"size:256"`
-	OwnerName string    `gorm:"size:100"`
-	Phone     string    `gorm:"size:30"`
+	ID        int `gorm:"primaryKey,not null,autoIncrement;uniqueIndex"`
+	ProductId int `gorm:"index"`
+	// Product   []Product     `gorm:"foreignKey:ID;references:ProductId;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	Location  string `gorm:"size:100"`
+	Name      string `gorm:"size:100"`
+	Desc      string `gorm:"size:256"`
+	OwnerName string `gorm:"size:100"`
+	Phone     string `gorm:"size:30"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt
