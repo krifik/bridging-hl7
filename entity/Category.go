eb455a6b5697f1fd4c3f9521a6a421e76e8e0b4b
Fikri Ilhamsyah <fiqriilhamsyah09@gmail.com>
2022-11-01 00:08:15 +0700

    change prefix product field

--- entity/Category.go
+++ entity/Category.go
@@ -1,16 +1,17 @@
 package entity
 
 import (
+	"database/sql"
 	"time"
 
 	"gorm.io/gorm"
 )
 
 type Category struct {
-	ID        int       `gorm:"primaryKey,not null,autoIncrement;uniqueIndex"`
-	Name      string    `gorm:"size:100"`
-	ProductId int       `gorm:"unique"`
-	Product   []Product `gorm:"foreignKey:ID;references:ProductId;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	ID        int           `gorm:"primaryKey,not null,autoIncrement;uniqueIndex"`
+	Name      string        `gorm:"size:100"`
+	ProductId sql.NullInt64 `gorm:"index"`
+	// Product   []Product     `gorm:"foreignKey:ID;references:ProductId;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt
