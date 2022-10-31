9b4e311b7809f8f3c9f4073967ea99c7d19fde1e
Fikri Ilhamsyah <fiqriilhamsyah09@gmail.com>
2022-11-01 00:09:53 +0700

    add belongs to association

--- entity/ProductImage.go
+++ entity/ProductImage.go
@@ -1,18 +1,20 @@
 package entity
 
 import (
+	"database/sql"
 	"time"
 
 	"gorm.io/gorm"
 )
 
 type ProductImage struct {
-	ID        int    `gorm:"primaryKey,not null,autoIncrement;uniqueIndex;"`
-	ProductId int    `gorm:"index"`
-	Path      string `gorm:"type:text"`
-	Large     string `gorm:"type:text"`
-	Medium    string `gorm:"type:text"`
-	Small     string `gorm:"type:text"`
+	ID        int           `gorm:"primaryKey,not null,autoIncrement;uniqueIndex;"`
+	ProductId sql.NullInt64 `gorm:"index"`
+	Product   []Product     `gorm:"foreignKey:ID;references:ProductId;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	Path      string        `gorm:"type:text"`
+	Large     string        `gorm:"type:text"`
+	Medium    string        `gorm:"type:text"`
+	Small     string        `gorm:"type:text"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt
