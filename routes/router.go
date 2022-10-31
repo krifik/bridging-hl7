aca7bc3e48df6ec3227a47cb9937f72d357f749c
Fikri Ilhamsyah <fiqriilhamsyah09@gmail.com>
2022-11-01 00:11:29 +0700

    add some endpoint

--- routes/router.go
+++ routes/router.go
@@ -7,10 +7,13 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func Route(app *fiber.App, controller controller.UserController) {
-	app.Get("/api/users", middleware.AuthMiddleware, controller.FindAll)
-	app.Post("/api/register", controller.Register)
-	app.Post("/api/login", controller.Login)
-	app.Post("/api/test", controller.TestRawSQL)
+func Route(app *fiber.App, userController controller.UserController, productController controller.ProductController) {
+	app.Get("/api/users", middleware.AuthMiddleware, userController.FindAll)
+	app.Post("/api/register", userController.Register)
+	app.Post("/api/login", userController.Login)
+	app.Post("/api/test", userController.TestRawSQL)
+	app.Get("/api/product/:id", productController.GetProduct)
+	app.Post("/api/product", productController.Save)
+	app.Get("/api/products", productController.GetProducts)
 	// app.Post("/api/users", middleware.AuthMiddleware, controller.Insert)
 }
