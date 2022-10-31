1d82edbc9c1b731d6087ce486e9243a3b4dcd61e
Fikri Ilhamsyah <fiqriilhamsyah09@gmail.com>
2022-11-01 00:09:10 +0700

    add nullable to foreign key

--- entity/Product.go
+++ entity/Product.go
@@ -1,6 +1,7 @@
 package entity
 
 import (
+	"database/sql"
 	"time"
 
 	"github.com/shopspring/decimal"
@@ -8,18 +9,15 @@ import (
 )
 
 type Product struct {
-	// gorm.Model
-	// Name string
-	// Type string
-	ID             int          `gorm:"primaryKey,not null,autoIncrement;uniqueIndex"`
-	ProductImageId int          `gorm:"unique"`
-	ProductImage   ProductImage `gorm:"foreignKey:ID;references:ProductImageId;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
-	CategoryId     int          `gorm:"unique"`
-	Category       Category     `gorm:"foreignKey:CategoryId;references:ID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
-	PartnerId      int          `gorm:"unique"`
-	Partner        Partner      `gorm:"foreignKey:PartnerId;references:ID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
-	Name           string       `gorm:"size:256;not null"`
-	Desc           string       `gorm:"size:256;not null"`
+	ID             int           `gorm:"primaryKey,not null,autoIncrement;uniqueIndex"`
+	CategoryId     sql.NullInt64 `gorm:"unique"`
+	Category       Category      `gorm:"foreignKey:CategoryId;references:ID;"`
+	PartnerId      sql.NullInt64 `gorm:"unique"`
+	Partner        Partner       `gorm:"foreignKey:PartnerId;references:ID;"`
+	ProductImageId sql.NullInt64 `gorm:"unique"`
+	ProductImage   ProductImage  `gorm:"foreignKey:ProductImageId;references:ID;"`
+	Name           string        `gorm:"size:256;not null"`
+	Desc           string        `gorm:"size:256;not null"`
 	Stock          int
 	Price          decimal.Decimal `gorm:"type:decimal(16,2)"`
 	CreatedAt      time.Time
