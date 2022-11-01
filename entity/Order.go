8ff0d610683a836e683fd6adbc54e81e0f1c51c2
Fikri Ilhamsyah <fiqriilhamsyah09@gmail.com>
2022-11-01 22:26:56 +0700

    delete constraint option and change unique > index

--- entity/Order.go
+++ entity/Order.go
@@ -8,15 +8,15 @@ import (
 )
 
 type Order struct {
-	ID        int     `gorm:"primaryKey,not null,autoIncrement;uniqueIndex"`
-	PaymentId int     `gorm:"unique"`
-	DriverId  int     `gorm:"unique"`
-	ProductId int     `gorm:"unique"`
-	UserId    int     `gorm:"unique"`
-	User      User    `gorm:"foreignKey:UserId;references:ID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
-	Payment   Payment `gorm:"foreignKey:ID;references:PaymentId;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
-	Driver    Driver  `gorm:"foreignKey:ID;references:DriverId;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
-	Product   Product `gorm:"foreignKey:ID;references:ProductId;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	ID        int `gorm:"primaryKey,not null,autoIncrement;uniqueIndex"`
+	PaymentId int `gorm:"index"`
+	DriverId  int `gorm:"index"`
+	ProductId int `gorm:"index"`
+	UserId    int `gorm:"index"`
+	User      User
+	Payment   Payment
+	Driver    Driver
+	// Product   Product `gorm:"foreignKey:ProductId;references:ID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 
 	TaxAmount       decimal.Decimal `gorm:"type:decimal(16,2)"`
 	TaxPercent      decimal.Decimal `gorm:"type:decimal(16,2)"`
