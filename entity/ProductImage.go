71800f35b18b08a8ad9c8da3842c262bb7e940d9
Fikri Ilhamsyah <fiqriilhamsyah09@gmail.com>
2022-11-01 22:27:52 +0700

    delete productId field

--- entity/ProductImage.go
+++ entity/ProductImage.go
@@ -1,15 +1,14 @@
 package entity
 
 import (
-	"database/sql"
 	"time"
 
 	"gorm.io/gorm"
 )
 
 type ProductImage struct {
-	ID        int `gorm:"primaryKey,not null,autoIncrement;uniqueIndex;"`
-	ProductId sql.NullInt64
+	ID int `gorm:"primaryKey,not null,autoIncrement;uniqueIndex;"`
+	// ProductId int
 	// Product   []Product `gorm:"foreignKey:ID;references:ProductId;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 	Path      string `gorm:"type:text"`
 	Large     string `gorm:"type:text"`
