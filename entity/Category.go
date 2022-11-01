0d2010ec3b92be09e4b4f5695c9c72e9fbe6596f
Fikri Ilhamsyah <fiqriilhamsyah09@gmail.com>
2022-11-01 22:26:17 +0700

    delete constraint option

--- entity/Category.go
+++ entity/Category.go
@@ -1,17 +1,16 @@
 package entity
 
 import (
-	"database/sql"
 	"time"
 
 	"gorm.io/gorm"
 )
 
 type Category struct {
-	ID        int           `gorm:"primaryKey,not null,autoIncrement;uniqueIndex"`
-	Name      string        `gorm:"size:100"`
-	ProductId sql.NullInt64 `gorm:"index"`
-	// Product   []Product     `gorm:"foreignKey:ID;references:ProductId;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	ID   int    `gorm:"primaryKey,not null,autoIncrement;uniqueIndex"`
+	Name string `gorm:"size:100"`
+	// ProductId sql.NullInt64 `gorm:"index"`
+	// Product   []Product     `gorm:"foreignKey:ProductId;"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt
