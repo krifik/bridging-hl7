eeb09efebf82f39ecf373cb5e233edd3a1b863f4
Fikri Ilhamsyah <fiqriilhamsyah09@gmail.com>
2022-11-04 16:47:06 +0700

    add api doc prefix

--- main.go
+++ main.go
@@ -3,9 +3,22 @@ package main
 import (
 	"flag"
 	"mangojek-backend/app"
+	_ "mangojek-backend/docs"
 	"mangojek-backend/exception"
 )
 
+// @title Mangojek API Docs
+// @version 1.0
+// @description This is an auto-generated API Docs.
+// @termsOfService http://swagger.io/terms/
+// @contact.name API Support
+// @contact.email your@mail.com
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
+// @BasePath /api
 func main() {
 	flag.Parse()
 	if arg := flag.Arg(0); arg != "" {
@@ -14,6 +27,6 @@ func main() {
 	}
 	app := app.InitializedApp()
 	// Start App
-	err := app.Listen(":3000")
+	err := app.Listen(":2000")
 	exception.PanicIfNeeded(err)
 }
