212d7d08cb89b7ddbb510fc2bff1704f6eb56345
Fikri Ilhamsyah <fiqriilhamsyah09@gmail.com>
2023-05-17 09:46:54 +0700

    url set from .env

--- config/rabbitmq.go
+++ config/rabbitmq.go
@@ -3,16 +3,22 @@ package config
 import (
 	"bridging-hl7/exception"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func InitializedRabbitMQ() (*amqp.Channel, *amqp.Connection) {
-	log.Println("Initializing RabbitMQ . . .")
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	err := godotenv.Load()
 	exception.SendLogIfErorr(err, "12")
-
+	url := os.Getenv("AMQP_URL")
+	log.Println("Initializing RabbitMQ . . .")
+	log.Println("URL : " + url)
+	conn, err := amqp.Dial(url)
+	exception.PanicIfNeeded(err)
+	exception.SendLogIfErorr(err, "17")
 	ch, err := conn.Channel()
-	exception.SendLogIfErorr(err, "15")
+	exception.SendLogIfErorr(err, "20")
 	return ch, conn
 }
