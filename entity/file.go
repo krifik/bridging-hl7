90a78cf4082e07c9b975ac7d4fe5a5c6878de80f
Fikri Ilhamsyah <fiqriilhamsyah09@gmail.com>
2023-07-03 10:00:52 +0700

    add modified column

--- entity/file.go
+++ entity/file.go
@@ -1,14 +1,17 @@
 package entity
 
 import (
+	"time"
+
 	"github.com/k0kubun/pp"
 	"gorm.io/gorm"
 )
 
 type File struct {
 	gorm.Model
-	FileName  string `gorm:"unique;size:256"`
-	ReadState bool   `gorm:"default:false"`
+	FileName     string `gorm:"unique;size:256"`
+	ReadState    bool   `gorm:"default:false"`
+	ModifiedTime time.Time
 }
 
 func Migrate(db *gorm.DB) {
